b9855a317491d6e86c58ac3bded51efdc63da5c7
Samuel Grayson <sam@samgrayson.me>
2024-10-14 17:38:04 -0500

    Bump version

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -ex
 
-mypy --strict main.py || true
-ruff check main.py
+mypy --strict --package repro_validator
+ruff check repro_validator
 if [ "$(grep $(cat version) README.md | wc -l)" -ne 2 ]; then
     echo 'Is README.md updated to refer to the new version?'
     exit
